c1a4baa8f75838e2f7a807c7e260ccfba7491cda
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-03-01 16:12:02 -0600

    Nueva funcion y class Arreglo

--- Test.java
+++ Test.java
@@ -1,11 +1,13 @@
+/*@By Andersson Jafett Beaz Estrada */
 import java.util.Scanner;
-
+//Maqueta o plantilla de mi clase Alumnos con sus metodos funciones publicas pero atributos privados
 class Alumnos{
     private String curp;
     private int edad;
     private String nombre;
     private String semestre;
 
+    //Constructor Alumnos
     public Alumnos(String curp, int edad, String nombre, String semestre){
         this.curp = curp;
         this.edad = edad;
@@ -13,6 +15,7 @@ class Alumnos{
         this.semestre = semestre;
     }
 
+    //metodos get para mostrar datos o valores almacenados
     public int getEdad(){
         return edad;
     }
@@ -26,6 +29,7 @@ class Alumnos{
     public String getSemestre(){
         return semestre;
     }
+    //validacion de que obj es instancia de Alumnos y es un objeto
 
     public boolean equals(Object obj) {
         if (obj == this) return true;
@@ -34,9 +38,29 @@ class Alumnos{
         return this.curp == alumno.curp;
     }
 
+    //metodo toString
     public String toString(){
-        return ("Curp "+ curp + "Edad " + edad+ "Nombre " + nombre + "Semestre "+semestre);
+        return ("Curp "+ curp + "Edad " + edad+ "Nombre " + nombre + "Semestre "+semestre); 
     }
+}
+
+abstract class Arreglos {
+    private Alumnos[] alumnos;
+    private int tamano;
+    private int i = 0;
+
+    public Arreglo(int tamano, int i,){
+        this.tamano = tamano;
+        this.i = i;
+    }
+    
+    public void agregarPrincipio(){
+        if (i == tamano){
+            
+        }
+    }
+
+    
 }
 
 
