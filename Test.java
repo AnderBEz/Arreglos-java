a26e983a2e46fecbe8373cb9ac8c4f3825171550
Andersson Beaz <andersson.beaz4313@gmail.com>
2024-03-01 14:55:07 -0600

    clase Alumnos lista y toString

--- Test.java
+++ Test.java
@@ -1,1 +1,52 @@
-ola
+import java.util.Scanner;
+
+class Alumnos{
+    private String curp;
+    private int edad;
+    private String nombre;
+    private String semestre;
+
+    public Alumnos(String curp, int edad, String nombre, String semestre){
+        this.curp = curp;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.semestre = semestre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public String getCurp(){
+        return curp;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public String getSemestre(){
+        return semestre;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Alumnos)) return false;
+        Alumnos alumno = (Alumnos) obj;
+        return this.curp == alumno.curp;
+    }
+
+    public String toString(){
+        return ("Curp "+ curp + "Edad " + edad+ "Nombre " + nombre + "Semestre "+semestre);
+    }
+}
+
+
+
+
+
+public class Test {
+    
+public static void main(String[] args) {
+    
+}
+    
+}
